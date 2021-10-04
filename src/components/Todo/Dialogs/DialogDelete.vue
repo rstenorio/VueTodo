e917eb1610c88810589eac16f111b14cf78e08a4
Rodrigo Tenorio <rodrigo.s.tenorio@gmail.com>
2021-10-04 12:06:05 +0200

    Dialog Delete Confirmation

---
+++ src/components/Todo/Dialogs/DialogDelete.vue
@@ -0,0 +1,37 @@
+<template>
+  <v-dialog 
+      :value="true" 
+      persistent max-width="290">
+    <v-card>
+      <v-card-title class="text-h5">
+        Delete Task?
+      </v-card-title>
+      <v-card-text
+        >Are you sure? </v-card-text
+      >
+      <v-card-actions>
+        <v-spacer></v-spacer>
+        <v-btn 
+            @click="$emit('close')">
+            No
+        </v-btn>
+        <v-btn 
+        @click="$store.dispatch('deleteTask', task.id)"
+        color="red lighten-1" 
+        text 
+        >
+          Yes
+        </v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
+</template>
+
+<script>
+export default {
+    props: ['task']
+};
+</script>
+
+<style>
+</style>
